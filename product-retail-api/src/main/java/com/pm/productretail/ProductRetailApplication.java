ea67f007e4f847bebd67054dbd6c2decea2e42e6
Liia <linu28@mail.ru>
2018-03-24 21:43:27 +0300

    initial

---
+++ product-retail-api/src/main/java/com/pm/productretail/ProductRetailApplication.java
@@ -0,0 +1,12 @@
+package com.pm.productretail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductRetailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductRetailApplication.class, args);
+	}
+}
