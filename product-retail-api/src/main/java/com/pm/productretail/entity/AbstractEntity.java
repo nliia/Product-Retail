229cf2d16b027dddeb864dbaf2eb481c03a9fb4b
Dmitry <flexusssss@gmail.com>
2018-03-24 23:17:23 +0300

    bug fix

--- product-retail-api/src/main/java/com/pm/productretail/entity/AbstractEntity.java
+++ product-retail-api/src/main/java/com/pm/productretail/entity/AbstractEntity.java
@@ -9,7 +9,7 @@ import javax.persistence.MappedSuperclass;
 @MappedSuperclass
 public abstract class AbstractEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     public Long getId() {
