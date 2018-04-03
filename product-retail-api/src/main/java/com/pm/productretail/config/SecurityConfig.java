1fc42bd19af096276ca42ed268da0262ec11fe8c
Liia <linu28@mail.ru>
2018-04-03 23:08:32 +0300

    cors added

--- product-retail-api/src/main/java/com/pm/productretail/config/SecurityConfig.java
+++ product-retail-api/src/main/java/com/pm/productretail/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
+        http.csrf().disable().cors().and().authorizeRequests()
                 .antMatchers("/**").permitAll()
                 .antMatchers("/department", "/task").authenticated()
                 .anyRequest().authenticated()
