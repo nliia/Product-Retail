e21e827c597edee5bf696ea44cba53dcb2a5f7b6
Liliya Mannapova <liliya.mf.18@gmail.com>
2018-05-11 20:34:43 +0300

    new menu item added

--- product-retail-frontend/src/components/Sidebar.vue
+++ product-retail-frontend/src/components/Sidebar.vue
@@ -51,6 +51,10 @@ export default {
         {
           name: 'Товары',
           path: 'goods'
+        },
+        {
+          name: 'Задачи',
+          path: 'tasks'
         }
       ]
       }
