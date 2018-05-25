621faaabf724bec9c7dab55724ac1942736107ef
Dmitry <flexusssss@gmail.com>
2018-05-25 23:08:45 +0300

    update messages date

--- product-retail-api/src/main/java/com/pm/productretail/dto/response/MessageResponseDto.java
+++ product-retail-api/src/main/java/com/pm/productretail/dto/response/MessageResponseDto.java
@@ -2,26 +2,31 @@ package com.pm.productretail.dto.response;
 
 import com.pm.productretail.entity.AppUser;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class MessageResponseDto {
 	private String text;
 	private AppUserResponseDto sender;
 	private AppUserResponseDto recipient;
-	private Date date;
+	private String date;
 
 	public MessageResponseDto(String text, AppUser sender, AppUser recipient, Date date) {
 		this.text = text;
 		this.sender = new AppUserResponseDto(sender);
 		this.recipient = new AppUserResponseDto(recipient);
-		this.date = date;
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+		dateFormat.setTimeZone(TimeZone.getTimeZone("GMT+03"));
+		this.date = dateFormat.format(date);
 	}
 
-	public Date getDate() {
+	public String getDate() {
 		return date;
 	}
 
-	public void setDate(Date date) {
+	public void setDate(String date) {
 		this.date = date;
 	}
 
